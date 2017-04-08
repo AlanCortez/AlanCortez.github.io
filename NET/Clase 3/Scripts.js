9ef49129b5b4991be6306fd50183d527f7100e0a
AlanCortez <alan.cortez@hotmail.com>
2017-04-08 10:50:54 -0500

    Tercera Clase de .NET

---
+++ NET/Clase 3/Scripts.js
@@ -0,0 +1,12 @@
+function mensaje()
+            {
+                var textbox=document.getElementById("textbox").value;
+                alert(textbox);
+            }
+function calc()
+            {
+                 var val1=Number(document.getElementById("textbox1").value);
+                 var val2=Number(document.getElementById("textbox2").value);
+                 var sum= val1 + val2;
+                 alert(sum);
+            }
